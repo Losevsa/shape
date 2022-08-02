f3033c9aac469a7f2f89dda94b14bdb7c1b3170c
Losevsa <losevsa@inbox.ru>
2022-08-02 07:53:22 +0300

    Добавьте файлы проекта.

---
+++ shape/shape.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+
+struct BoundingBoxDimensions
+{
+    int height, width;
+};
+
+class Shape
+{
+public:
+
+    virtual void squareCount() = 0;
+    virtual void printParams(Shape* shape) = 0;
+    virtual double square() = 0;
+    virtual BoundingBoxDimensions dimensions() = 0;
+    virtual std::string type() = 0;
+};
+
+class Circle : virtual public Shape
+{
+public:
+	Circle(){}
+
+    virtual void squareCount()
+    {
+
+    }
+    virtual void printParams(Shape* shape)
+    {
+        std::cout << "Type: " << shape->type() << std::endl;
+    }
+    virtual double square()
+    {
+
+    }
+    virtual BoundingBoxDimensions dimensions()
+    {
+
+    }
+    virtual std::string type()
+    {
+        return "Circle";
+    }
+
+private:
+    double a;
+    double b;
+    double c;
+};
+
+
+int main()
+{
+
+    std::cout << "Hello World!\n";
+}
+
